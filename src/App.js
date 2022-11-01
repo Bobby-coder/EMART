efb0eeedf999654bceb8a9df65e9854ec4bb79a0
Bobby-coder <bobbysadhwani612@gmail.com>
2022-11-01 21:59:38 +0530

    Diffrent routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <></>
+    <>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='product' element={<Product />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='cart' element={<Cart />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
